0dd20c4c53d0f788539a04d276326b108e74aba2
Hironori Fujimoto <fujimoto-weldyn@outlook.jp>
2021-02-17 13:03:54 +0000

    Add node list model

---
+++ ros2_top/node_list.py
@@ -0,0 +1,40 @@
+class NodeList:
+    def __init__(self, node):
+        self._node = node
+            self._node_list = []
+
+        for name in self._node.get_node_names_and_namespaces():
+            node_info = new NodeInfo(node, name[0], name[1])
+            self._node_list.append(node_info)
+
+class NodeInfo:
+    def __init__(self, node, name, namespace):
+        self._node = node
+        self.name = name
+        self.namespace = namespace
+
+        self.count_publish_topics()
+        self.count_subscribe_topics()
+
+    def count_publish_topics(self):
+        publish_topics = self._node.get_publisher_names_and_types_by_node(
+            self.name, self.namespace)
+        self.publish_count = len(publish_topics)
+
+        # Number of publish topic which has subscribers
+        self.connected_publish_count = 0
+        for topic in publish_topics:
+            if (self._node.get_subscriptions_info_by_topic(topic[0]) > 0):
+                connected_publish_count++;
+
+    def count_subscribe_topics(self):
+        subscribe_topics = self._node.get_subscriptions_info_by_topic(
+            self.name, self.namespace)
+        self.subscribe_count = len(subscribe_topics)
+
+        # Number of publish topic which has subscribers
+        self.connected_subscribe_count = 0
+        for topic in subscribe_topics:
+            if (self._node.get_subscriptions_info_by_topic(topic[0]) > 0):
+                connected_subscribe_count++;
+
