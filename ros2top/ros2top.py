9af00c403d6d236214aac4f5ecbb0a9407e0c91c
Hironori Fujimoto <fujimoto-weldyn@outlook.jp>
2021-02-21 18:57:31 +0900

    Fix node name and delete unused code

--- ros2top/ros2top.py
+++ ros2top/ros2top.py
@@ -10,20 +10,17 @@ import pprint
 
 class Ros2Top(Node):
     def __init__(self):
-        super().__init__('ros2_top')
+        super().__init__('ros2top')
         with ManagedScreen() as screen:
             view = NodeListView(screen)
             model = NodeListModel(self)
             view.update_model(model)
-
-            self.get_logger().info(pprint.pformat(model.node_list))
 
             screen.play([Scene([view], -1)])
 
 def main(args=None):
     rclpy.init(args = args);
     top = Ros2Top()
-    rclpy.spin(top)
 
     top.destroy_node()
     rclpy.shutdown()
