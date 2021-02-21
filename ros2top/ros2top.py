c4c6b248d895e28cf2cdc516163ba871340f6591
Hironori Fujimoto <fujimoto-weldyn@outlook.jp>
2021-02-21 20:17:05 +0900

    Add parameter

--- ros2top/ros2top.py
+++ ros2top/ros2top.py
@@ -1,16 +1,22 @@
 import rclpy
 from rclpy.node import Node
+from rcl_interfaces.msg import IntegerRange, ParameterDescriptor, ParameterType
+
 from asciimatics.screen import ManagedScreen
 from asciimatics.scene import Scene
 
 from ros2top.view.node_list_view import NodeListView
 from ros2top.model.node_list_model import NodeListModel
 
-import pprint
-
 class Ros2Top(Node):
     def __init__(self):
         super().__init__('ros2top')
+
+        update_rate = self.declare_parameter('update_rate', 1, ParameterDescriptor(
+            type=ParameterType.PARAMETER_INTEGER,
+            description='Update rate for list by seconds',
+            integer_range=[IntegerRange(from_value=1, to_value=20, step=1)]))
+
         with ManagedScreen() as screen:
             view = NodeListView(screen)
             model = NodeListModel(self)
