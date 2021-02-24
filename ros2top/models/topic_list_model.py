bcbe522cce6c1ed1b243d4352d1bfb7e4afae80c
Hironori Fujimoto <fujimoto-weldyn@outlook.jp>
2021-02-24 22:02:51 +0900

    Add TopicListModel

---
+++ ros2top/models/topic_list_model.py
@@ -0,0 +1,18 @@
+import ros2topic.api
+from rclpy.node import Node
+from collections import namedtuple
+
+TopicSummary = namedtuple('TopicSummary', ['name', 'types', 'publisher_count', 'subscriber_count'])
+
+class TopicListModel:
+    def __init__(self, node: Node):
+        self.list = []
+        names_and_types = ros2topic.api.get_topic_names_and_types(node=node)
+        
+        for (name, types) in names_and_types:
+            list.append(TopicSummary(
+                name,
+                types,
+                node.count_publishers(name),
+                node.count_subscribers(name)
+            ))
